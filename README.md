b9de6ad37a89520ba8c8d91adac2387a504b5a42
cavisd7 <cavisd7@gmail.com>
2020-12-21 22:37:30 -0500

    readme

---
+++ README.md
@@ -0,0 +1,4 @@
+# Wasm-8
+A chip-8 emulator for the browser.
+
+![](./docs/images/wasm8.png)
--- src/components/Controls.tsx
+++ src/components/Controls.tsx
@@ -13,18 +13,28 @@ export class Controls extends Component<Props, State> {
                 <h3>Controls</h3>
                 <div className='keys'>
                     <div className='key-group'>
-                        <p><kbd>w</kbd> - up</p>
-                        <p><kbd>a</kbd> - left</p>
-                        <p><kbd>d</kbd> - right</p>
-                        <p><kbd>s</kbd> - down</p>
+                        <p><kbd>1</kbd> - ???</p>
+                        <p><kbd>2</kbd> - ???</p>
+                        <p><kbd>3</kbd> - ???</p>
+                        <p><kbd>4</kbd> - ???</p>
                     </div>
                     <div className='key-group'>
-                        <p><kbd>space</kbd> - jump</p>
-                        <p><kbd>space</kbd> - jump</p>
+                        <p><kbd>q</kbd> - ???</p>
+                        <p><kbd>w</kbd> - ???</p>
+                        <p><kbd>e</kbd> - ???</p>
+                        <p><kbd>r</kbd> - ???</p>
                     </div>
                     <div className='key-group'>
-                        <p><kbd>space</kbd> - jump</p>
-                        <p><kbd>space</kbd> - jump</p>
+                        <p><kbd>a</kbd> - ???</p>
+                        <p><kbd>s</kbd> - ???</p>
+                        <p><kbd>d</kbd> - ???</p>
+                        <p><kbd>f</kbd> - ???</p>
+                    </div>
+                    <div className='key-group'>
+                        <p><kbd>z</kbd> - ???</p>
+                        <p><kbd>x</kbd> - ???</p>
+                        <p><kbd>c</kbd> - ???</p>
+                        <p><kbd>v</kbd> - ???</p>
                     </div>
                 </div>
             </div>
